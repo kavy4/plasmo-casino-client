7f1b42ceac45f8d13173a732d69a7e83d22492bf
kavy4 <tyty020408@gmail.com>
2023-01-15 18:54:05 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,6 @@ function Header({ balance, children }) {
   return (
 		<header><Link className='header-link' to='/'>{ children }</Link>Баланс: {balance}<button style={{margin: '0'}}>Пополнить</button></header>
 	)
-}
+}	
 
 export default Header
